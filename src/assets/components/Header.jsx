032123814045c17caa03da684eb6b533b04f7d58
Daud Abdiwahab Elmoge <daudwahab1320@gmail.com>
2025-04-23 18:28:46 +0300

    edited header

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="bg-black text-white shadow-md py-4 px-6">
+    <header className="bg-black text-white shadow-md py-4 px-6 sticky top-0 z-50 ">
       <div className="max-w-7xl mx-auto flex flex-col sm:flex-row items-center justify-between">
         {/* Logo and Title */}
         <div className="flex items-center space-x-4 mb-4 sm:mb-0">
           <img
-            src="https://images.ctfassets.net/1nvpgv2kdfc0/51dciW7FJ8Olsi1Wnsg16y/f6c4c29caa98d517f0563b27e678fcc9/Buses_in_the_United_States.jpg"
+            src="/images/Zuru Shuttles.png"
             alt="Zuru Shuttles Logo"
-            className="h-12 w-12 rounded-full object-cover border-2 border-yellow-400"
+            className="w-35 h-35 object-contain rounded-full border-2 border-yellow-400"
           />
           <h1 className="text-xl md:text-2xl font-bold text-yellow-400">
             Zuru Shuttles
